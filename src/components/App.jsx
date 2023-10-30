73ed1b7ccfb065d2596b66177f00c8fc0eab6758
kayavalentina <kayavalentina1993@gmail.com>
2023-10-30 21:18:37 +0100

    Task 4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,19 +4,22 @@ import { Statistics } from './Statistics/Statistics';
 import data from '../data.json';
 import { FriendList } from './FriendList/FriendList';
 import friends from '../friends.json';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+import transactions from '../transactions.json';
 
 export const App = () => {
   return (
     <div>
-          <Profile
+        <Profile
         username={user.username}
         tag={user.tag}
         location={user.location}
         avatar={user.avatar}
         stats={user.stats} />
-       <Statistics title="Upload stats" stats={data} />
+        <Statistics title="Upload stats" stats={data} />
         <Statistics stats={data} />
         <FriendList friends={friends} />
+        <TransactionHistory items={transactions} />
       </div>   
   );
 };
---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,24 @@
+import css from '../TransactionHistory/TransactionHistory.module.css';
+
+export const TransactionHistory = ({ items }) => {
+  return (
+    <table className={css.table}>
+      <thead className={css.tableHead}>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+      <tbody>
+        {items.map(({id, type, amount, currency}) => (
+          <tr key={id} className={css.tableItem}>
+            <td>{type}</td>
+            <td>{amount}</td>
+            <td>{currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
